05cbe65a45b261fcfccee14c9085085fe3bc782d
yukims19 <yukims19@gmail.com>
2019-01-31 00:46:50 -0800

    Refetch PR comments

--- pages/docs/api/v2/api-docs-mdx/endpoints/commentBox.jsx
+++ pages/docs/api/v2/api-docs-mdx/endpoints/commentBox.jsx
@@ -1,8 +1,83 @@
 import { UL, LI } from '~/components/list'
+import Card from '~/components/card'
+import { Code } from '~/components/text/code'
+import Example from '~/components/example'
+import ChatCount from '~/components/chat-count'
+
+const commentPRMutation = `mutation commentPR($body:String!, $subjectId: String!){
+  gitHub {
+    addComment(
+      input: {
+        body: $body
+        subjectId: $subjectId
+      }
+    ) {
+      clientMutationId
+    }
+  }
+}
+`
+const getSinglePRQuery = `query singlePullRequests(
+  $name: String!
+  $owner: String!
+  $number: Int!
+) {
+  gitHub {
+    repository(name: $name, owner: $owner) {
+      pullRequest(number: $number) {
+        id
+        comments(last: 20) {
+          nodes {
+            id
+            bodyText
+            author {
+              login
+              avatarUrl
+            }
+            createdAt
+          }
+        }
+      }
+    }
+  }
+}
+`
 
 class CommentBox extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      comment: null,
+      commentsNodes: this.props.prInfo.comments.nodes
+    }
+  }
+
+  updateComment(value) {
+    this.setState({ comment: value })
+  }
+
+  commentPR(auth, repoOwner, repoName, body, focusedPR, graphqler) {
+    graphqler(auth, commentPRMutation, {
+      body: body,
+      subjectId: focusedPR.id
+    }).then(data => {
+      graphqler(auth, getSinglePRQuery, {
+        owner: repoOwner,
+        name: repoName,
+        number: this.props.prInfo.number
+      }).then(json => {
+        console.log('comment return data:', json)
+        if (json.errors) {
+          alert('Error!! Could not comment.')
+        } else {
+          this.setState({
+            comment: '',
+            commentsNodes:
+              json.data.gitHub.repository.pullRequest.comments.nodes
+          })
+        }
+      })
+    })
   }
 
   render() {
